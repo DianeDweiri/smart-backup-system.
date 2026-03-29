077ad0e50f0e25c0dee2edbce0acddcbf643fa3f
Diane Dweiri <daiandnh8@gmail.com>
2026-03-29 05:17:12 +0300

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,13 +1,10 @@
-import time
-import json
+import time,datetime,shutil,logging,smtplib,schedule,json
 from pathlib import Path
-import datetime
-import shutil
-import logging
-import smtplib
-import schedule
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
+from watchdog.events import FileSystemEventHandler
+from watchdog.observers import Observer
+
 
 CONFIG_FILE = Path("config.json")
 def load_config():
@@ -33,6 +30,33 @@ logging.basicConfig(
 )
 
 EMAIL = config["email"]
+
+def handle_file(path):
+    file = Path(path)
+
+    if not file.exists() or not file.is_file():
+        return
+
+    for folder, extensions in config["file_types"].items():
+        if file.suffix.lower() in extensions:
+
+            dest = Path(config["destination_folder"]) / folder
+            dest.mkdir(parents=True, exist_ok=True)
+
+            dest_file = dest / file.name
+
+            counter = 1
+            while dest_file.exists():
+                dest_file = dest / f"{file.stem}_{counter}{file.suffix}"
+                counter += 1
+
+            try:
+                shutil.move(str(file), str(dest_file))
+                logging.info(f"{file.name} moved to {folder}")
+            except Exception as e:
+                logging.error(f"{file.name} failed: {e}")
+
+            break
 
 def backup_and_notify():
 
@@ -71,16 +95,24 @@ def backup_and_notify():
     except Exception as e:
         logging.error(f"Email sending failed: {e}")
 
+class WatchHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if not event.is_directory:
+            time.sleep(2)
+            handle_file(event.src_path)
+
 schedule.every(config["schedule_days"]).days.do(backup_and_notify)
 
-def run_scheduler():
-    logging.info("Backup scheduler started")
-
-    while True:
-        schedule.run_pending()
-        time.sleep(30)
-
-
 if __name__ == "__main__":
-    backup_and_notify()
-    run_scheduler()
+    observer = Observer()
+    event_handler = WatchHandler()
+
+    observer.schedule(event_handler, path=str(SRC_FOLDER), recursive=False)
+    observer.start()
+    try:
+        while True:
+            schedule.run_pending()
+            time.sleep(3)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
